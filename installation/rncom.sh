297af0822283869abe33162bcd324ee1b8ce9db7
gadepall <gadepall@gmail.com>
2021-07-14 11:39:13 +0530

    Add existing files

---
+++ installation/rncom.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+git clone https://github.com/gadepall/training $PREFIX/../home/storage/shared/training
+
+
+
+# cd $PREFIX/../home/storage/shared/training
+# texfot pdflatex gvv_math_eg.tex
+# texfot pdflatex gvv_math_eg.tex
