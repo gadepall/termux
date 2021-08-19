aab94020ac72f1ace2b431bd983dfedfe3bb6051
gadepall <gadepall@gmail.com>
2021-08-19 19:26:12 +0530

    Add existing files

--- installation/rncom.sh
+++ installation/rncom.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 
-git clone https://github.com/gadepall/training $PREFIX/../home/storage/shared/training
+#Download python and latex templates
+
+git clone https://github.com/gadepall/training /data/data/com.termux/files/home/storage/shared/training
 
 
+#Test Python Installation
+#Uncomment only the following line
+#python3 /data/data/com.termux/files/home/storage/shared/training/math/codes/tri_sss.py
 
-# cd $PREFIX/../home/storage/shared/training
-# texfot pdflatex gvv_math_eg.tex
-# texfot pdflatex gvv_math_eg.tex
+#Test Latex Installation
+#Uncomment only the following lines
+#cd /data/data/com.termux/files/home/storage/shared/training/math
+#texfot pdflatex gvv_math_eg.tex
+#texfot pdflatex gvv_math_eg.tex
