6ee3c70b65dbea4842d791d5909b40d373378f62
gadepall <gadepall@gmail.com>
2021-09-12 15:45:43 +0530

    Add existing files

---
+++ installation/.ipynb_checkpoints/rncom-checkpoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#Download python and latex templates
+
+git clone https://github.com/gadepall/training /data/data/com.termux/files/home/storage/shared/training
+
+
+#Test Python Installation
+#Uncomment only the following line
+#python3 /data/data/com.termux/files/home/storage/shared/training/math/codes/tri_sss.py
+
+#Test Latex Installation
+#Uncomment only the following lines
+#cd /data/data/com.termux/files/home/storage/shared/training/math
+#texfot pdflatex gvv_math_eg.tex
+#texfot pdflatex gvv_math_eg.tex
