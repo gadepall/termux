c59afb1dbb28958a3ebf41f80707db7842f4a914
gadepall <gadepall@gmail.com>
2021-10-28 07:06:55 +0530

    Add existing files

--- termuxrpi/al-wpa-setup.sh
+++ termuxrpi/al-wpa-setup.sh
@@ -4,13 +4,10 @@
 
 #usage:  
 #chmod +x al-wpa-setup.sh
-#./al-wpa-setup.sh /dev/sdb wifi_name wifi_password
+#./al-wpa-setup.sh  wifi_name wifi_password
 
 #Adapted from
 #https://ladvien.com/installing-arch-linux-raspberry-pi-zero-w/
-#for android termux
-#you need to enter the ssid and passwd manually so that  you  may connect
-#to your rpi wirelessly from the phone
 
 #!/bin/sh -ex
 
@@ -22,6 +19,6 @@ set -e
 SSID="$1"
 PASS="$2"
 
-wpa_passphrase "${SSID}" "${PASS}" > root/etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+wpa_passphrase "${SSID}" "${PASS}" >> /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 
 
